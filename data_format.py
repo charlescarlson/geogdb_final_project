c856a16a975d00d929f11fcbeb16a5d7c690adfd
charlescarlson <charles-carlson@uiowa.edu>
2018-10-17 13:01:19 -0500

    Adding more csv creation scripts

--- data_format.py
+++ data_format.py
@@ -4,25 +4,51 @@ import os
 
 def create_csv(table):
     csv_path = 'csv_files'
+    new_csv = open('%s/%s.csv' % (csv_path, table), 'w', encoding='utf-8')
     if not os.path.isdir(csv_path):
         os.mkdir(csv_path)
     with open('Chicago_Crimes_2012_to_2017.csv', 'r', encoding="utf-8") as file:
         csv_reader = csv.reader(file, delimiter=',')
         if table = 'crime':
-            new_csv = open('%s/%s.csv' % (csv_path, table), 'w', encoding='utf-8')
+            crime = []
             for row in csv_reader:
                 if row[3].split(' ')[0][-1] == '5':
-                    new_csv.write(','.join(row) + '\n')
+                    crime.append([row[1], row[2], row[3], row[9], row[10]])
+            for row in crime:
+                new_csv.write(','.join(row) + '\n')
         if table = 'crime_location':
-            pass
+            crime_locations = []
+            for row in csv_reader:
+                if row[3].split(' ')[0][-1] == '5':
+                    crime_locations.append([row[2], row[7], row[4], row[22], row[13]])
+            for row in crime_locations:
+                new_csv.write(','.join(row) + '\n')
         if table = 'crime_type':
-            pass
+            crime_types = []
+            for row in csv_reader:
+                if row[3].split(' ')[0][-1] == '5':
+                    crime_types.append([row[5], row[6], row[7]])
+            for row in crime_types:
+                new_csv.write(','.join(row) + '\n')
         if table = 'instace_of':
             pass
         if table = 'ward':
-            pass
+            wards = []
+            for row in csv_reader:
+                if row[3].split(' ')[0][-1] == '5':
+                    wards.append([row[13]])
+            for row in wards:
+                new_csv.write(','.join(row) + '\n')
             
 if __name__ == "__main__":
-    crime_table = sys.argv[1]
-    
-    create_csv(crime_table)
+    if sys.argv[1] == '--all':
+        create_csv('crime')
+        create_csv('crime_location')
+        create_csv('crime_type')
+        create_csv('instance_of')
+        create_csv('ward')
+    if len(sys.argv) == 2:
+        create_csv(sys.argv[1])
+    else:
+        for arg in sys.argv[1:]:
+            create_csv(arg)
