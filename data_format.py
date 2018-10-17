67abd5807fe926b56082ae476383f7dc6e0310e3
Charles Carlson <charles-carlson@uiowa.edu>
2018-10-17 00:33:41 -0500

    ENH: Adding Python skeleton code for other CSVs

--- data_format.py
+++ data_format.py
@@ -6,13 +6,21 @@ def create_csv(table):
     csv_path = 'csv_files'
     if not os.path.isdir(csv_path):
         os.mkdir(csv_path)
-    new_csv = open('%s/%s.csv' % (csv_path, table), 'w', encoding='utf-8')
     with open('Chicago_Crimes_2012_to_2017.csv', 'r', encoding="utf-8") as file:
         csv_reader = csv.reader(file, delimiter=',')
-        line = 0
-        for row in csv_reader:
-            if row[3].split(' ')[0][-1] == '5':
-                new_csv.write(','.join(row) + '\n') 
+        if table = 'crime':
+            new_csv = open('%s/%s.csv' % (csv_path, table), 'w', encoding='utf-8')
+            for row in csv_reader:
+                if row[3].split(' ')[0][-1] == '5':
+                    new_csv.write(','.join(row) + '\n')
+        if table = 'crime_location':
+            pass
+        if table = 'crime_type':
+            pass
+        if table = 'instace_of':
+            pass
+        if table = 'ward':
+            pass
             
 if __name__ == "__main__":
     crime_table = sys.argv[1]
