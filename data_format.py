ddb10540987285e0d1eb778e7ef86c4c45c853fa
charlescarlson <charles-carlson@uiowa.edu>
2018-10-23 00:31:15 -0500

    some changes

--- data_format.py
+++ data_format.py
@@ -4,35 +4,35 @@ import os
 
 def create_csv(table):
     csv_path = 'csv_files'
-    new_csv = open('%s/%s.csv' % (csv_path, table), 'w', encoding='utf-8')
     if not os.path.isdir(csv_path):
         os.mkdir(csv_path)
+    new_csv = open('%s/%s.csv' % (csv_path, table + "_staging"), 'w', encoding='utf-8')
     with open('Chicago_Crimes_2012_to_2017.csv', 'r', encoding="utf-8") as file:
         csv_reader = csv.reader(file, delimiter=',')
-        if table = 'crime':
+        if table == 'crime':
             crime = []
             for row in csv_reader:
                 if row[3].split(' ')[0][-1] == '5':
                     crime.append([row[1], row[2], row[3], row[9], row[10]])
             for row in crime:
                 new_csv.write(','.join(row) + '\n')
-        if table = 'crime_location':
+        if table == 'crime_location':
             crime_locations = []
             for row in csv_reader:
                 if row[3].split(' ')[0][-1] == '5':
                     crime_locations.append([row[2], row[7], row[4], row[22], row[13]])
             for row in crime_locations:
                 new_csv.write(','.join(row) + '\n')
-        if table = 'crime_type':
+        if table == 'crime_type':
             crime_types = []
             for row in csv_reader:
                 if row[3].split(' ')[0][-1] == '5':
                     crime_types.append([row[5], row[6], row[7]])
             for row in crime_types:
                 new_csv.write(','.join(row) + '\n')
-        if table = 'instace_of':
+        if table == 'instace_of':
             pass
-        if table = 'ward':
+        if table == 'ward':
             wards = []
             for row in csv_reader:
                 if row[3].split(' ')[0][-1] == '5':
